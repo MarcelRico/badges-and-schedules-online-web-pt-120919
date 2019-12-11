416f5c7d88fa16121ee963830b0908a5ae309ce8
Marcel Rico <personal.mr4@gmail.com>
2019-12-11 15:58:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
-  
+def batch_badge_creator(names)
+  badge_message = []
+  names.collect{|name| badge_message << badge_maker(name)}
+  badge_message
 end
