8c95d66fa42938e915b575befe4d0b7e0aa9eea1
Marcel Rico <personal.mr4@gmail.com>
2019-12-11 15:49:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,27 +1,3 @@
-# Write your code here.
-
-def badge_maker(name)
-  "Hello, my name is #{name}."
-end
-
-def batch_badge_creator(names)
-  badges = []
-  names.collect{|name| badges << badge_maker(name)}
-  badges
-end
-
-def assign_rooms(speakers)
-  rooms_assignment = []
-  
-  speakers.length.times do |idx|
-    speaker  = speakers[idx]
-    phrase = "Hello, #{speaker}! You'll be assigned to room #{idx+1}!"
-    rooms_assignment << phrase
-  end
-  rooms_assignment
-end
-
-def printer(names)
-  puts batch_badge_creator(names)
-  puts assign_rooms(names)
+def conference_badges(name)
+  puts "Hello, my name is #{name}."
 end
